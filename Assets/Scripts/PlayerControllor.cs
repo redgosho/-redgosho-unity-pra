4d9d53cf78cc46e6e999e1fe415e47b9380463a7
redgosho <redgosho@gmail.com>
2020-01-13 19:34:53 +0900

    UnityチュートリアルUnit1を終了段階　操作スクリプトは多少オリジナル

---
+++ Assets/Scripts/PlayerControllor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllor : MonoBehaviour
+{
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        // 車が前後に動く
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // 車が左右に曲がる。動いてない時は回転しない
+        if (forwardInput > 0)
+        {
+            transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+        } else if(forwardInput < 0)
+        {
+            transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime * -1);
+        }
+    }
+}
